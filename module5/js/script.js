2630ff0913bb88d60be1c0008206e143fee4ff60
Vladyslav Kochetkov <me@vladk.dev>
2021-04-27 16:25:19 -0400

    module5 wip

--- module5/js/script.js
+++ module5/js/script.js
@@ -95,14 +95,11 @@ $(function () {
     const x = $ajaxUtils.sendGetRequest(
       homeHtmlUrl,
       function (homeHtml) {
-        console.log(homeHtml);
         var randomCategory = chooseRandomCategory(categories);
-        console.log(randomCategory);
         // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
         var chosenCategoryShortName = randomCategory.short_name;
-        console.log(chosenCategoryShortName);
         // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
         // chosen category from STEP 2. Use existing insertProperty function for that purpose.
         // Look through this code for an example of how to do use the insertProperty function.
@@ -113,6 +110,13 @@ $(function () {
         // $dc.loadMenuItems('L')
         // Hint: you need to surround the chosen category short name with something before inserting
         // it into the home html snippet.
+
+        var homeHtmlToInsertIntoMainPage = homeHtml.replaceAll(
+          /{{randomCategoryShortName}}/g,
+          chosenCategoryShortName
+        );
+
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
         //
         // var homeHtmlToInsertIntoMainPage = ....
         // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
